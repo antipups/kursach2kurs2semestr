4c08d950f3f1c7a4a5591c49694bb23f09303031
antipups <nikitakurkurin@protonmail.ch>
2020-02-13 01:16:05 +0300

    Создал формочку для кнопок (добавлени/удаление/ещё что-то)

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -2,12 +2,17 @@ from django.urls import path
 from django.views.generic import ListView, DetailView
 from .models import *
 
-
 urlpatterns = [
     path('',
-         ListView.as_view(queryset=(('Добавить в', 'Аптеки', 'Фирмы', 'Партии'),
-                                    ('Удалить из', 'аптек', 'фирм', 'партий'),
-                                    ('Изменить в', 'аптеках', 'фирмах', 'партиях')),
+         ListView.as_view(queryset=({"Buttons":
+                                         (('Добавить в', 'Аптеки', 'Фирмы', 'Партии'),
+                                          ('Удалить из', 'аптек', 'фирм', 'партий'),
+                                          ('Изменить в', 'аптеках', 'фирмах', 'партиях')),
+                                     "DB":
+                                         Pharmacy.objects.all(),
+                                     "Mods":
+                                         ("Работа с базой данных (Добавление, удаление)", "Задания"),
+                                     }),
                           template_name='content.html'),
          name='hw'),
 ]
